9fb33ba13d4d2a20d6fecb77bd36f1c4383ef75b
alkemann <alek.morland@gmail.com>
2010-01-10 19:14:50 +0100

    refactored the form view for `Plugin`

--- views/plugins/form.html.php
+++ views/plugins/form.html.php
@@ -1,80 +1,84 @@
 <?php
 
-echo $this->form->create(null, array('url' => array('plugin' => 'li3_lab', 'action' => 'verify')));
+$errors = $plugin->errors();
+echo $this->form->create($plugin, array('url' => array('plugin' => 'li3_lab', 'action' => 'verify')));
 
 $this->form->config(array('templates' => array('checkbox' =>
 	'<input type="hidden" name="{:name}" value="0" />
 	 <input type="checkbox" value="1" name="{:name}"{:options} />'
 )));
 
-if (isset($plugin->id) && isset($plugin->rev)) : ?>
-	<input type="hidden" name="id" value="<?=$plugin->id;?>" />
-	<input type="hidden" name="rev" value="<?=$plugin->rev;?>" />
-<?php endif;?>
-<p>
+if (isset($plugin->id) && isset($plugin->rev)) {
+	echo $this->form->hidden('id');
+	echo $this->form->hidden('rev');
+}
+?>
+<div class="input">
 	<?php
 		echo $this->form->label('name', 'Plugin Name', array(
 			'class' => 'required'
 		));
-		echo $this->form->text('name', array(
-			'value' => $plugin->name
-		));
-		if (isset($plugin->errors['name'])) {
-			echo '<p style="color:red">' . $plugin->errors['name'] . '</p>';
+		echo $this->form->text('name');
+		if (isset($errors['name'])) {
+			echo '<p style="color:red">' . implode(', ', $errors['name']) . '</p>';
 		}
 	?>
-</p>
-<p>
+</div>
+<div class="input">
 	<?php
 		echo $this->form->label('version', 'Version', array(
 			'class' => 'required'
 		));
-		echo $this->form->text('version', array(
-			'value' => $plugin->version
-		));
-		if (isset($plugin->errors['version'])) {
-			echo '<p style="color:red">' . $plugin->errors['version'] . '</p>';
+		echo $this->form->text('version');
+		if (isset($errors['version'])) {
+			echo '<p style="color:red">' . implode(', ', $errors['version']) . '</p>';
 		}
 	?>
-</p>
-<p>
+</div>
+<div class="input">
 	<?php
 		echo $this->form->label('summary', 'Summary', array(
 			'class' => 'required'
 		));
-		echo $this->form->textarea('summary', array(
-			'value' => $plugin->summary, 'cols' => 40, 'rows' => 5
-		));
-		if (isset($plugin->errors['summary'])) {
-			echo '<p style="color:red">' . $plugin->errors['summary'] . '</p>';
+		echo $this->form->textarea('summary', array('cols' => 40, 'rows' => 5));
+		if (isset($errors['summary'])) {
+			echo '<p style="color:red">' . implode(', ', $errors['summary']) . '</p>';
 		}
 	?>
-</p>
-<p>
+</div>
+<div class="input">
 	<?php
 		echo $this->form->label('description', 'Description');
-		echo $this->form->textarea('description', array(
-			'value' => $plugin->description, 'cols' => 40, 'rows' => 15
-		));
-		if (isset($plugin->errors['description'])) {
-			echo '<p style="color:red">' . $plugin->errors['description'] . '</p>';
+		echo $this->form->textarea('description', array('cols' => 40, 'rows' => 15));
+		if (isset($errors['description'])) {
+			echo '<p style="color:red">' . implode(', ', $errors['description']) . '</p>';
 		}
 	?>
-</p>
-<p>
+</div>
+<div class="input">
 	<?php
 		echo $this->form->label('source', 'Source Path', array(
 			'class' => 'required'
 		));
-		echo $this->form->text('source', array(
-			'value' => $plugin->source
-		));
-		if (isset($plugin->errors['source'])) {
-			echo '<p style="color:red">' . $plugin->errors['source'] . '</p>';
+		echo $this->form->text('source');
+		if (isset($errors['source'])) {
+			echo '<p style="color:red">' . implode(', ', $errors['source']) . '</p>';
 		}
 	?>
-</p>
+</div>
+<div class="input">
+	<fieldset id="maintainers">
+		<legend>Maintainers</legend>
+	<?php
+		if (isset($errors['maintainers'])) {
+			echo '<div style="color:red">' . implode(', ', $errors['maintainers']) . '</div>';
+		}
+		echo $this->maintainer->render($plugin->maintainers);
+	?>
+	</fieldset>
+	<a href="#add-maintainer" id="add-maintainer">Add maintainer</a>
+</div>
 <?php echo $this->form->submit('save', array('name' => 'verified'));?>
-<?php echo $this->form->submit('cancel', array('name' => 'cancel'));?>
+<?php echo $this->form->submit('cancel', array('value' => 'cancel'));?>
 </form>
 <p></p>
